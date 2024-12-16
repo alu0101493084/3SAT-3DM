3d7953a12760e19ac01305707439e9813847b869
Diego Antonio Pi Arteaga <91694143+alu0101493084@users.noreply.github.com>
2024-12-16 21:05:58 +0000

    Implementada la impresión de threeDM

--- src/ThreeDM.cpp
+++ src/ThreeDM.cpp
@@ -3,3 +3,27 @@
 ThreeDM::ThreeDM(int group_size) {
     this->group_size = group_size;
 }
+
+std::ostream& operator<<(std::ostream& out, const ThreeDM& t_dm) {
+    std::cout << "SETS\n\n";
+    std::cout << "W { ";
+    for (int i = 0; i < t_dm.group_size - 1; i++) {
+        std::cout << t_dm.matchings[i].w << ", ";
+    }
+    std::cout << t_dm.matchings[t_dm.matchings.size() - 1].w << " }\n\n";
+    std::cout << "X { ";
+    for (int i = 0; i < t_dm.group_size - 1; i++) {
+        std::cout << t_dm.matchings[i].x << ", ";
+    }
+    std::cout << t_dm.matchings[t_dm.matchings.size() - 1].x << " }\n\n";
+    std::cout << "Y { ";
+    for (int i = 0; i < t_dm.group_size - 1; i++) {
+        std::cout << t_dm.matchings[i].y << ", ";
+    }
+    std::cout << t_dm.matchings[t_dm.matchings.size() - 1].y << " }\n\n";
+    std::cout << "M {\n";
+    for (int i = 0; i < t_dm.group_size - 1; i++) {
+        std::cout << "(" << t_dm.matchings[i].w << ", " << t_dm.matchings[i].x << ", " << t_dm.matchings[i].y << "),\n";
+    }
+    std::cout << "(" << t_dm.matchings[t_dm.matchings.size() - 1].w << ", " << t_dm.matchings[t_dm.matchings.size() - 1].x << ", " << t_dm.matchings[t_dm.matchings.size() - 1].y << ")\n}";
+}
