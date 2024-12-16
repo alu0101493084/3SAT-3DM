d4144ccc6180037cbe928a1ac3b0a20b59a9afa0
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2024-12-16 18:04:30 +0000

    more reduction

--- src/ReductionThreeDM.cpp
+++ src/ReductionThreeDM.cpp
@@ -10,8 +10,8 @@ ThreeDM ReductionThreeDM::reduce(const ThreeSAT& t_sat) {
     std::vector<Matching> matchings;
 
     // Starting indexes of elements for each component
-    int element_st = t_sat.get_variable_amount() * t_sat.get_clause_amount();
-    int element_gc = element_st + t_sat.get_clause_amount();
+    const int element_st = t_sat.get_variable_amount() * t_sat.get_clause_amount();
+    const int element_gc = element_st + t_sat.get_clause_amount();
 
     // Truth setting and fan-out
     for (int star = 0; star < t_sat.get_variable_amount(); star++) {
@@ -20,8 +20,20 @@ ThreeDM ReductionThreeDM::reduce(const ThreeSAT& t_sat) {
     }
 
     // Satisfactory testing
+    for (int c = 0; c < t_sat.get_clause_amount(); c++) {
+        Clause clause = t_sat.get_clauses()[c];
+        for (Literal l : clause.literals) {
+            int star_index = l.variable * 2 * t_sat.get_clause_amount();
+            int spike_index = star_index + c + !l.value;
+            Matching m = {spike_index, element_st + c, element_st + c};
+            matchings.push_back(m);
+        }
+    }
 
     // Garbage collector
+    const int gc_size = 2 * t_sat.get_clause_amount() * (t_sat.get_variable_amount() - 1);
+    for (int gc = 0; gc < gc_size; gc++) {
+    }
 
     return t_dm;
 }
