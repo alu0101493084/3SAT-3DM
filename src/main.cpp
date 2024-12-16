7be87c57b1957656e820dcf207b5a92b02ab08b1
Diego Antonio Pi Arteaga <91694143+alu0101493084@users.noreply.github.com>
2024-12-16 18:55:20 +0000

    Creado objeto ReductionThreeDM

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 #include "../include/ThreeSAT.hpp"
+#include "../include/ThreeDM.hpp"
+#include "../include/ReductionThreeDM.hpp"
 
 int main (int argc, char *argv[]) {
+    ReductionThreeDM problem_reduction;
     ThreeSAT three_sat("3sat.json");
+    ThreeDM three_dm(2);
+    problem_reduction.reduce(three_sat);
     std::cout << three_sat << std::endl;
     return 0;
 }
