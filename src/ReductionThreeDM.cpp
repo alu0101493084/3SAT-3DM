b8540abd151f98f6035bcd55548454c2bbbda0bd
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2024-12-16 15:41:06 +0000

    start reduction

--- src/ReductionThreeDM.cpp
+++ src/ReductionThreeDM.cpp
@@ -9,8 +9,18 @@ ThreeDM ReductionThreeDM::reduce(const ThreeSAT& t_sat) {
     ThreeDM t_dm(2 * t_sat.get_variable_amount() * t_sat.get_clauses().size());
     std::vector<Matching> matchings;
 
+    // Starting indexes of elements for each component
+    int element_st = t_sat.get_variable_amount() * t_sat.get_clause_amount();
+    int element_gc = element_st + t_sat.get_clause_amount();
+
     // Truth setting and fan-out
+    for (int star = 0; star < t_sat.get_variable_amount(); star++) {
+        for (int spike = 0; spike < 2 * t_sat.get_clause_amount(); spike++) {
+        }
+    }
+
     // Satisfactory testing
+
     // Garbage collector
 
     return t_dm;
